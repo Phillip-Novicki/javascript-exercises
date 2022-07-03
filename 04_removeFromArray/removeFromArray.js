3d820648065819643efba5193c9fd5463c19f72d
Phillip Novicki <Phillip.Novicki@gmail.com>
2022-07-03 13:38:45 -0500

    Completed removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(arr)
+{
+        for (let a = 1; a <= arguments.length; a++){
+            num = arguments[a];
+        
+            for (let i = 0; i <= arr.length; i++){
+                if (arr[i] === num){
+                    arr.splice(i, 1);
+                }
+            }
+        }
+    return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
