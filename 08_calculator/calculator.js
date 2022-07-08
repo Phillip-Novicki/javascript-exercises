db4c7511dbce0b31929808a32049c0a4becbcec3
Phillip Novicki <Phillip.Novicki@gmail.com>
2022-07-08 10:20:17 -0500

    calculator completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,67 @@
+//create simple addition function
 const add = function() {
+  let sum = 0;
+  for (i = 0; i < arguments.length; i++){
+    sum += arguments[i];
+  }
+
+  return sum;
 	
 };
 
+//create subtraction function
 const subtract = function() {
+	let diff = arguments[0];
+  for (i = 1; i < arguments.length; i++){
+    diff -= arguments[i];
+  }
+
+  return diff;
+};
+
+//convert array into sum
+const sum = function(array) {
+  let sum = 0;
+  let nums = array;
+  if (!arguments.length){
+    return sum;
+  }
+  for (i = 0; i < nums.length; i++){
+    sum += nums[i];
+  }
+
+  return parseInt(sum);
+  };
+
+// convert array into product  
+const multiply = function(array) {
+  let product = 1;
+  let nums = array;
+  for (i = 0; i < nums.length; i++){
+    product *= nums[i];
+  }
+  return product;
+
+};
+
+// multiply num (exp amount of times)
+const power = function(num, exp) {
+  let solution = 1;
+  for (i = 0; i < exp; i++){
+    solution *= num;
+  } 
+
+  return solution;
 	
 };
 
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+// count from 1 to number using i
+const factorial = function(number) {
+  let solution = 1;
+  for (let i = 1; i <= number; i++){
+    solution *= i;
+  }
+  return solution;
 	
 };
 
