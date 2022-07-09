dd51acdc981a747071eca1c5b9b66a78cc800e83
Phillip Novicki <Phillip.Novicki@gmail.com>
2022-07-09 14:10:34 -0500

    completed palindrome with comments

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function(inp) {
+    // convert all letters to lower case
+    let oldWord = inp.toLowerCase();
 
-};
-
+    // remove special characters
+    let word = oldWord.replace(/[^a-zA-Z0-9]/g, "");
+    // define length of loop 
+    const times = word.length; 
+        for (let i = 0; i < (times / 2); i++){
+            if (word[i].valueOf() === word[(times - 1 - i)].valueOf()){
+                continue;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+}
 // Do not edit below this line
 module.exports = palindromes;
